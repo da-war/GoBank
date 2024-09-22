673344a6c6b67bd7fab2a791e7c8cee83513e3cf
drkast <imdawar090@gmail.com>
2024-09-22 06:32:05 +0500

    if

--- bank.go
+++ bank.go
@@ -4,4 +4,32 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, World!")
+	fmt.Println("Welcome to GoBank!")
+	fmt.Println("What would you like to do?")
+	fmt.Println("Please select an option:")
+	fmt.Println("1. Create a new account")
+	fmt.Println("2. Deposit money")
+	fmt.Println("3. Withdraw money")
+	fmt.Println("4. Check balance")
+	fmt.Println("5. Exit")
+
+	var option int
+	fmt.Scanln(&option)
+
+	if option == 1 {
+		fmt.Println("Creating a new account...")
+	}
+	if option == 2 {
+		fmt.Println("Depositing money...")
+	}
+	if option == 3 {
+		fmt.Println("Withdrawing money...")
+	}
+	if option == 4 {
+		fmt.Println("Checking balance...")
+	}
+	if option == 5 {
+		fmt.Println("Exiting...")
+	}
+
 }
